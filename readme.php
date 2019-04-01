43df6385802a79109077067c1464cc6067814968
jose manuel alfonso <jose.manuelchema111@gmail.com>
2019-04-01 22:29:17 +0200

    readme added

---
+++ readme.php
@@ -0,0 +1,22 @@
+## 1-reto.php
+
+Dada una serie de palabras separadas por espacios, escribir la frase formada por las mismas palabras en orden inverso. 
+Cada palabra estará formada exclusivamente por letras, y existirá exactamente un espacio entre cada pareja de palabras.
+La salida debe ser "Case #" seguido del número de caso, de un símbolo de "dos puntos", de un espacio en blanco y de la frase invertida.
+
+
+## 2-reto.php
+
+Un entero se dice que es un palíndromo si es igual al número que se obtiene al invertir el orden de sus cifras.
+Por ejemplo, 79197 y 324423 son palíndromos. 
+
+En esta tarea se le dará un entero N, 1 <= N <= 1.000.000 
+Usted debe encontrar el menor entero M tal que M >= Nm M es primo y es un palíndromo.
+
+*Por ejemplo, si N es 31, entonces la respuesta es 101.
+
+Formato de entrada:
+Un solo entero N, (1 <= N <= 1.000.000), en una sola línea.
+ 
+Formato de salida:
+Su salida debe consistir en un solo número entero, el más pequeño palíndromo primo mayor que o igual a N.
